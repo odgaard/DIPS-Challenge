4ba8d7d3ce6478b111803251d990eefed2e591d2
odgaard <jacob.torring@gmail.com>
2017-09-14 19:23:26 +0200

    Implementing more error handling and renaming

--- DIPS-Challenge/Bank.cs
+++ DIPS-Challenge/Bank.cs
@@ -12,27 +12,42 @@ namespace DIPS_Challenge
 
         public Account CreateAccount(Person customer, Money initialDeposit)
         {
-            if (!_requestOwnerHasSufficientFunds(customer, initialDeposit))
+            if (_validPersonWithdrawTransaction(customer, initialDeposit))
             {
-                throw new ArgumentOutOfRangeException("Owner has insufficient funds: " + customer.Money.Value + " < " + initialDeposit.Value);
+                var newAccount = new Account(initialDeposit, customer);
+                customer.Money = new Money(customer.Money.Value - initialDeposit.Value);
+                customer.AddAccounts(newAccount);
+                return newAccount;
             }
-            var newAccount = new Account(initialDeposit, customer);
-            customer.Money = new Money(customer.Money.Value - initialDeposit.Value);
-            customer.AddAccounts(newAccount);
-            return newAccount;
+            return null;
         }
 
         public Account[] GetAccountsForCustomer(Person customer) => customer.Accounts;
 
         // This method only supports one type of currency.
-        private bool _requestOwnerHasSufficientFunds(Person owner, Money amount) => (owner.Money.Value >= amount.Value);
+        private bool _requestPersonHasSufficientFunds(Person owner, Money amount) => (owner.Money.Value >= amount.Value);
 
         // This method only supports one type of currency.
         private bool _requestAccountHasSufficientFunds(Account transfer, Money amount) => (transfer.Money.Value >= amount.Value);
 
-        private bool _requestMoneyIsPositive(Money amount) => (amount.Value > 0); 
+        private bool _requestMoneyIsPositive(Money amount) => (amount.Value > 0);
 
-        private bool _validWithdrawTransaction(Account transfer, Money amount)
+        private bool _validPersonWithdrawTransaction(Person owner, Money amount)
+        {
+            if (!_requestPersonHasSufficientFunds(owner, amount))
+            {
+                throw new ArgumentOutOfRangeException("Person has insufficient funds: " + owner.Money.Value + " < " + amount.Value);
+            }
+
+            if (!_requestMoneyIsPositive(amount))
+            {
+                throw new ArgumentOutOfRangeException("Invalid value, Negative " + amount.Value);
+            }
+
+            return true;
+        }
+
+        private bool _validAccountWithdrawTransaction(Account transfer, Money amount)
         {
             if (!_requestAccountHasSufficientFunds(transfer, amount))
             {
@@ -47,7 +62,7 @@ namespace DIPS_Challenge
             return true;
         }
 
-        private bool _validDepositTransaction(Account transfer, Money amount)
+        private bool _validAccountDepositTransaction(Account transfer, Money amount)
         {
             if (!_requestMoneyIsPositive(amount))
             {
@@ -57,38 +72,38 @@ namespace DIPS_Challenge
             return true;
         }
 
-        private bool _validTransferTransaction(Account from, Account to, Money amount) => (
-                _validWithdrawTransaction(from, amount) &&
-                _validDepositTransaction(to, amount)
+        private bool _validAccountTransferTransaction(Account from, Account to, Money amount) => (
+                _validAccountWithdrawTransaction(from, amount) &&
+                _validAccountDepositTransaction(to, amount)
                 );
 
         // This method only supports one type of currency.
-        private void _performDepositTransaction(Account transfer, Money amount) => 
+        private void _performAccountDepositTransaction(Account transfer, Money amount) => 
             transfer.Money = new Money(transfer.Money.Value + amount.Value);
 
         // This method only supports one type of currency.
-        private void _performWithdrawTransaction(Account transfer, Money amount) =>
+        private void _performAccountWithdrawTransaction(Account transfer, Money amount) =>
             transfer.Money = new Money(transfer.Money.Value - amount.Value);
 
         public void Deposit(Account to, Money amount)
         {
-            if (_validDepositTransaction(to, amount))
+            if (_validAccountDepositTransaction(to, amount))
             {
-                _performDepositTransaction(to, amount);
+                _performAccountDepositTransaction(to, amount);
             }       
         }
 
         public void Withdraw(Account from, Money amount)
         {
-            if(_validWithdrawTransaction(from, amount))
+            if(_validAccountWithdrawTransaction(from, amount))
             {
-                _performWithdrawTransaction(from, amount);
+                _performAccountWithdrawTransaction(from, amount);
             }
         }
 
         public void Transfer(Account from, Account to, Money amount)
         {
-            if(_validTransferTransaction(from, to, amount))
+            if(_validAccountTransferTransaction(from, to, amount))
             {
                 Withdraw(from, amount);
                 Deposit(to, amount);
